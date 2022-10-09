2f4948223c97244621727de2cdf444806dde105a
Yeliazar <erik.ruzhakov@mail.ru>
2022-10-09 15:55:02 +0300

    add loading state and error handling

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
 import Card from '../UI/Card';
+import loadingIndicator from "../UI/LoadingIndicator";
 import './IngredientForm.css';
+import LoadingIndicator from "../UI/LoadingIndicator";
 
 const IngredientForm = React.memo(props => {
   const [ enteredTitle, setEnteredTitle ] =  useState('');
@@ -41,6 +43,7 @@ const IngredientForm = React.memo(props => {
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
+              {props.loading && <LoadingIndicator/>}
           </div>
         </form>
       </Card>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,11 +2,14 @@ import React, {useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 import Search from './Search';
 
 const Ingredients = () => {
 
     const [userIngredients, setUserIngredients] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
 
     useEffect(() => {
         console.log('Render ingredients', userIngredients);
@@ -17,11 +20,13 @@ const Ingredients = () => {
     }, []);
 
     const addIngredientHandler = ingredient => {
+        setIsLoading(true);
         fetch('https://redux-toolkit-1111b-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json', {
             method: 'POST',
             body: JSON.stringify(ingredient),
             headers: {'Content-Type': 'application/json'}
         }).then(response => {
+            setIsLoading(false);
             return response.json();
         }).then(responseData => {
             setUserIngredients(prevIngredients => [
@@ -32,17 +37,31 @@ const Ingredients = () => {
     };
 
     const removeIngredientHandler = ingredientId => {
+        setIsLoading(true);
         fetch(`https://redux-toolkit-1111b-default-rtdb.europe-west1.firebasedatabase.app/ingredients/${ingredientId}.json`, {
             method: 'DELETE',
         }).then(response => {
+            setIsLoading(false);
             setUserIngredients(prevIngredients => prevIngredients.filter((i) => i.id !== ingredientId)
             );
-        })
+        }).catch(error => {
+            setError(error.message);
+            setIsLoading(false);
+        });
+    };
+
+    const clearError = () => {
+        setError(null);
     }
 
     return (
         <div className="App">
-            <IngredientForm onAddIngredient={addIngredientHandler}/>
+            { error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+
+            <IngredientForm
+                onAddIngredient={addIngredientHandler}
+                loading={isLoading}
+            />
 
             <section>
                 <Search onLoadIngredients={filteredIngredientsHandler}/>
